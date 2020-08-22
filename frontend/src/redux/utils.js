f48111ddd35671538bbe4de0681d98419032dbbe
Yash Shah <yashah1234@gmail.com>
2020-08-23 02:50:01 +0530

    added minified to getUserFromUserId

--- frontend/src/redux/utils.js
+++ frontend/src/redux/utils.js
@@ -1,4 +1,12 @@
 import store from "./store";
 
-export const getUserFromUserId = (userId) =>
-  userId > 10001 ? store.getState().user.users[userId] : null;
+export const getUserFromUserId = (userId, minified = false) => {
+  const user = userId > 10001 ? store.getState().user.users[userId] : null;
+  if (!user) return user;
+  if (minified) {
+    const minifiedUser = {};
+    minifiedUser["fullname"] = user["fullname"];
+    minifiedUser["person_no"] = user["person_no"];
+    return minifiedUser;
+  } else return user;
+};
