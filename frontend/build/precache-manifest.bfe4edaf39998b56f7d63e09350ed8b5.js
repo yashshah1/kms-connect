647ee5668e99d133335748be594496f524a52f6e
Yash Shah <yashah1234@gmail.com>
2020-09-18 11:43:28 +0530

    Build on 18.09 1143

---
+++ frontend/build/precache-manifest.bfe4edaf39998b56f7d63e09350ed8b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c84ef56d580d61ae330542d3f98d36d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c8673a271138ecb3711",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5b4ca1110e24f145342e",
+    "url": "/static/css/main.6b4e2bec.chunk.css"
+  },
+  {
+    "revision": "4c8673a271138ecb3711",
+    "url": "/static/js/2.80ddf94f.chunk.js"
+  },
+  {
+    "revision": "78cd68464d9142e5e4ec4b0c9fe54db7",
+    "url": "/static/js/2.80ddf94f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4ca1110e24f145342e",
+    "url": "/static/js/main.018a03b3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
